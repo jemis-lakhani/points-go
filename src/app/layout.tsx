cb8abd66eea69eed54cedb91c1eb831a83b51cba
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-09-04 19:45:36 +0530

    fix added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import "rsuite/dist/rsuite.css";
+import ClientProvider from "./ClientProvider";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClientProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClientProvider>
   );
 }
